b167f341ecf9fdac22921a8a99bd28b8f5639bb9
dbc2201 <divyanshb22@gmail.com>
2020-02-13 07:24:24 +0530

    add JavaDoc comments for the add method

--- src/adt/QueueADT.java
+++ src/adt/QueueADT.java
@@ -1,6 +1,12 @@
 package adt;
 
 public interface QueueADT<E> {
+    /**
+     * Inserts item at the rear of the queue.
+     *
+     * @param item the data that is to be stored in the node.
+     * @return true when the node is added to the queue.
+     */
     boolean add(E item);
 
     E remove();
